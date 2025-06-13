ae73787d3f907da8c137aaea1dee0e8be769b45b
Roma Zbyshko <roma.zb7@gmail.com>
2025-06-13 17:10:29 -0400

    Add files via upload

--- homework 03-1.py
+++ homework 03-1.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 
 def get_days_from_today(date_str):
-    #поточна дата
-    current_date = datetime.today()
-    # перетворення рядка дати в об'єкт datetime
-    specified_date = datetime.strptime(date_str, "%Y-%m-%d")
-    # обчислення різниці в днях
-    delta = specified_date - current_date
-    # повертаємо різницю в днях
-    return delta.days
+    try:
+        #поточна дата
+        current_date = datetime.today().date()
+        # перетворення рядка дати в об'єкт datetime
+        specified_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        # обчислення різниці в днях
+        delta = specified_date - current_date
+        # повертаємо різницю в днях
+        return delta.days
+    except ValueError:
+        return "Невірний формат дати. Використовуйте YYYY-MM-DD."
 
-print(get_days_from_today("2024-06-24"))  # Виведе кількість днів до 31 жовтня 2023 року
+print(get_days_from_today("2025-06-13"))  # Виведе кількість днів до 31 жовтня 2023 року
 
---
+++ homework 03-2.py
@@ -0,0 +1,13 @@
+import random
+def get_numbers_ticket(min_val, max_val, quantity):
+    if min_val < 1 or max_val > 1000 or min_val > max_val:
+        return []
+    
+    if quantity > (max_val - min_val + 1):
+        return []
+    
+    numbers = random.sample(range(min_val, max_val + 1), quantity)
+    return sorted(numbers)    
+
+lottery_numbers = get_numbers_ticket(2,50, 9)
+print("Ваші лотарейні числа", lottery_numbers)
